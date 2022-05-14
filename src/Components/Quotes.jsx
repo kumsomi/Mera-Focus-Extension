0e7484c972e21314882ed234924fc3e6b3bc6863
Kum Somi <kumsomi.5@gmail.com>
2022-05-14 16:55:16 +0530

    added Quotes
    Added quotes fetched from typefit API

--- src/Components/Quotes.jsx
+++ src/Components/Quotes.jsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
+
 function Quote(){
+    const [quote, setQuote]=useState("");
+    const [author, setAuthor]=useState("");
+
+    // this number will shuffle between the first n quotes and display any one among those n
+    const randomQuoteNumber = Math.floor(Math.random()*10);
+
+    const quotesApi="https://type.fit/api/quotes";
+    useEffect(()=>{
+        (
+            async () => {
+            try {
+              const res = await fetch(quotesApi);
+              const { status, ...data } = await res.json();
+              if (!res.ok) throw new Error({status});
+              setQuote(data[randomQuoteNumber].text);
+              setAuthor(data[randomQuoteNumber].author);
+            } catch (error) {
+              console.log("Error while getting quotes: ", error);
+              setQuote({ text: "Not able to fetch the quotes." });
+            } 
+          })();
+    },[]);
+
     return (
-        <div>
-            <h1>Quote</h1>
+        <div className="quote-fix">
+            <div>Quote:-{quote}</div>
+            <div>-{author}</div>
         </div>
     )
 }

8728b001912f32d873d4a326a4505fc20f6fe7ba
Kum Somi <kumsomi.5@gmail.com>
2022-05-18 14:26:30 +0530

    Add: added clock
    - also added the icon and title

---
+++ src/Context/TimeContext.js
@@ -0,0 +1,48 @@
+import { useState, createContext, useContext } from "react";
+
+const defaultTIme= new Date();
+const TimeContext = createContext(defaultTIme);
+
+const useTime = () => useContext(TimeContext);
+
+
+const newTime=new Date();
+
+//british time--> en-GB -->24hrs format
+const nowTime = newTime.toLocaleTimeString("en-GB");
+const hr=Number(nowTime.slice(0,2));
+
+function changePartOfDay(){
+    //checking with the hour hand by taking the first 2 letters out 13:32:29 --> slice(0,2)
+    //5am to 12pm -> morning
+    if(hr>=4 && hr<12){
+        return "morning";
+    }
+    if(hr>=12 && hr<17){
+        return "afternoon";
+    }
+    if(hr>=17 && hr<19){
+        return "evening";
+    }
+    if(hr>=19 && hr>4){
+        return "night";
+    }
+}
+    function TimeProvider({ children }) {
+        const [displayTime, setDisplayTime] = useState("");
+        const [partOfDay, setPartOfDay] = useState(changePartOfDay);
+      
+        setInterval(() => {
+          const newTime = new Date();
+          setDisplayTime(newTime.toLocaleTimeString("en-GB"));
+          setPartOfDay(changePartOfDay());
+        }, 1000);
+      
+        return (
+          <TimeContext.Provider value={{ displayTime, partOfDay }}>
+            {children}
+          </TimeContext.Provider>
+        );
+      }
+      
+export { TimeProvider, useTime };
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { TimeProvider } from './Context/TimeContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <TimeProvider>
     <App />
+    </TimeProvider>
   </React.StrictMode>
 );
 
